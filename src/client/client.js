0de6b0749ca103d65b1375a2daaf9dca80da8a8c
hsa1280 <shianhuang2008@gmail.com>
2019-03-09 13:41:12 -0800

    Added route to server side

--- src/client/client.js
+++ src/client/client.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/home';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
-ReactDOM.hydrate(<Home />, document.getElementById("root"));
+ReactDOM.hydrate(
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
+  document.getElementById("root"));
